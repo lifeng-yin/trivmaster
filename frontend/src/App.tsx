708404361e03ecc73831c48a869f086a5efa0d9c
Li Feng Yin <lifeng-yin@proton.me>
2023-05-13 21:56:10 -0400

    front: add react-router, add pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./components/Home"
+import Join from "./components/Join"
+import Room from "./components/Room"
+
 const App = () => {
 
-  return (<></>)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/room/:id" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
