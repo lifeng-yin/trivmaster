c0b88a4fd6da19a9a5d15249aa0d05cb3d7c7a6f
Li Feng Yin <lifeng-yin@proton.me>
2023-05-15 20:43:39 -0400

    back: use dotenv, use env port variables, use cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,22 @@
+import dotenv from 'dotenv'
 import express, { Express } from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 
+dotenv.config()
+
+const client_port = process.env.CLIENT_PORT || 5173
+const server_port = process.env.SERVER_PORT || 5000
+
 const app: Express = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:' + client_port
+  }
+})
 
-server.listen(5000, () => {
-  console.log('🚀 Backend server is up and running!')
+
+server.listen(server_port, () => {
+  console.log(`🚀 Backend server is up and running on port ${server_port}!`)
 })
