8e550f3b4189e0a8e5749c5d7765cfb2c512dab2
Li Feng Yin <lifeng-yin@proton.me>
2023-05-16 17:49:51 -0400

    front: connect to backend using socket.io-client

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,6 +1,21 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import { io } from 'socket.io-client'
+
 function Room() {
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  if (!id) {
+    navigate('/join')
+  }
+  
+  const socket = io(import.meta.env.VITE_SERVER_URL)
+  console.log(socket)
+  
   return (
-    <div>Room</div>
+    <div>
+      <h1>Room {id}</h1>
+    </div>
   )
 }
 
