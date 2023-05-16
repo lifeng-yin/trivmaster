d4162387859b4b67cfa2d98fedc35906582fc2fd
Li Feng Yin <lifeng-yin@proton.me>
2023-05-15 21:53:34 -0400

    front: add join page frontend logic

--- frontend/src/components/Join.tsx
+++ frontend/src/components/Join.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 function Join() {
+  const navigate = useNavigate()
+  const [roomId, setRoomId] = useState('')
+
+  const handleSubmit = () => {
+    navigate(`/room/${roomId}`)
+  }
+
   return (
-    <div>Join</div>
+    <div>
+      <h1>Join a room</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={roomId} onChange={e => setRoomId(e.target.value)}></input>
+        <button disabled={roomId === ''} type="submit">Join</button>
+      </form>
+    </div>
   )
 }
 
