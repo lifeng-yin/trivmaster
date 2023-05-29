81629d31f0923f994bec7f8c4c74c75611c38bc8
Li Feng Yin <lifeng-yin@proton.me>
2023-05-28 22:09:30 -0400

    front: fix edit round not appearing

--- frontend/src/components/Room/EditQuestions.tsx
+++ frontend/src/components/Room/EditQuestions.tsx
@@ -1,4 +1,6 @@
-const Questions = () => {
+import { Socket } from "socket.io-client"
+
+const Questions = ({ socket, roomId }: { socket: Socket, roomId: string }) => {
   return <>
     <h1 className="text-center text-2xl font-bold">Edit Questions</h1>
   </>
